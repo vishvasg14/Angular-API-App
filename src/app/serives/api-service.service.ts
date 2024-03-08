555e303928be513f09cd4676b840ad2f51cdbd00
Vishvas Gohil <122722469+vishvasg14@users.noreply.github.com>
2024-03-08 18:00:59 +0530

    Update api-service.service.ts
    Update api link

--- src/app/serives/api-service.service.ts
+++ src/app/serives/api-service.service.ts
@@ -9,7 +9,7 @@ export class ApiServiceService {
   constructor(private _http:HttpClient) { }
 
   getdata(){
-    return this._http.get('https://apigenerator.dronahq.com/api/39KrdvyX/data');
+    return this._http.get('https://apigenerator.dronahq.com/api/0FfAJCJH/data');
   }
   
   
